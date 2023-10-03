a462e40962ecc1acf1443d152b00384cc7932991
STSC <pardhu.dhoni@gmail.com>
2023-10-02 20:35:02 -0700

    Lesson 3.5 end

---
+++ Day2/03_05/script.js
@@ -0,0 +1,26 @@
+/**
+ * Create a Backpack object.
+ */
+
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  lidOpen: false,
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+};
+console.log("The Dot notation access for pocket numbers:",backpack.pocketNum);
+console.log("the bracket notation access for pocket num:",backpack["pocketNum"]);
+var query = "pocketNum";
+console.log("the bracket notation access for pocket num:",backpack[query]);
