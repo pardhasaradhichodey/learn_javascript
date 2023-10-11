b11ef02c364e7019d55a4696b334b90ad03f515e
STSC <pardhu.dhoni@gmail.com>
2023-10-11 15:08:51 -0700

    Practice 5_4 end

--- Day3/05_04/script.js
+++ Day3/05_04/script.js
@@ -2,3 +2,6 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+console.log(document.querySelectorAll("article"));
+console.log(document.querySelectorAll(".backpack__name"));
+console.log(document.querySelector(".backpack li:last-child"));
