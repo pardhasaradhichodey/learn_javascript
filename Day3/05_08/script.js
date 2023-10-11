39b98d9506b99b08bf5e1730bda099e34e469e21
STSC <pardhu.dhoni@gmail.com>
2023-10-11 15:44:25 -0700

    Practice 5_8 end

---
+++ Day3/05_08/script.js
@@ -0,0 +1,15 @@
+/**
+ * Note: This file is intentionally empty.
+ * You can use it to test your skills at adding, removing, or modifying
+ * attributes, classes, and inline styles on elements.
+ */
+var x=document.querySelector("figure").classList
+console.log(x)
+x.add("pardhu");
+console.log(x)
+x.remove('backpack__image');
+console.log(x)
+x.toggle('backpack__image');
+console.log(x);
+x.toggle('pardhu');
+console.log(x);
